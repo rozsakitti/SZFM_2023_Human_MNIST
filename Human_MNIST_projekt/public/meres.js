180685d558954df6d44d804a03cb41ed1a7db219
Halász Kata <halasz.kata01@gmail.com>
2023-11-26 01:05:27 +0100

    Kitti kódrészleteinek ráfésülése az adatbázisos kódokra

--- Human_MNIST_projekt/public/meres.js
+++ Human_MNIST_projekt/public/meres.js
@@ -1,18 +1,7 @@
-let images = []; // Array to store image names
 let currentIndex = 0;
 
-//const button = document.getElementById('btn');
-
-//button.addEventListener('click', event => {
-//    console.log('button clicked');
-//});
-
-
-
-
-//document.getElementById("result").value = filename;
-buttonHandler()
-GetImageName()
+buttonHandler();
+GetImageName();
 
 function buttonHandler() {
     var points = 0;
@@ -23,34 +12,37 @@ function buttonHandler() {
     var uniqdate = Date.now();
 
     for (var i = 0; i < allButtons.length; i++) {
-        allButtons[i].addEventListener('click', function() {
-        clicked = this.innerHTML;
-        console.log("You clicked:", clicked);
-        sumpoints=sumpoints+1
-        console.log("First letter=" + GetFirstLetter(document.getElementById("mnistImage").src))
-        console.log("GetImageName" + GetImageName())
-        if (this.innerHTML == GetImageName()) {
-            points=points+1
-            console.log("asd" + points)
+        allButtons[i].addEventListener('click', function () {
+            clicked = this.innerHTML;
+            console.log("You clicked:", clicked);
+            sumpoints = sumpoints + 1;
+            console.log("First letter=" + GetFirstLetter(document.getElementById("mnistImage").src));
+            console.log("GetImageName" + GetImageName());
+            if (this.innerHTML == GetImageName()) {
+                points = points + 1;
+                console.log("asd" + points);
+            }
+            fetchData().then(arr => {
+                var pic_url = arr[getRandomInt(5000)];
+                var prev_pic = document.getElementById("mnistImage").src;
+                document.getElementById("mnistImage").src = "auth/img/mnist/" + pic_url;
+                console.log(pic_url);
+                InsertData(prev_pic.split('\\').pop().split('/').pop(), clicked, uniqdate);
+            });
 
-        }
-        fetchData().then(arr => {
-            var pic_url = arr[getRandomInt(5000)]
-            var prev_pic = document.getElementById("mnistImage").src
-            document.getElementById("mnistImage").src="auth/img/mnist/" + pic_url
-            console.log(pic_url)
-            InsertData(prev_pic.split('\\').pop().split('/').pop(), clicked, uniqdate)
+            currentIndex++; // Increment the current index
+            document.getElementById("currentIndex").innerText = currentIndex;
+
+            console.log(points + "/" + sumpoints);
         });
-        console.log(points + "/" + sumpoints)
-    });
-  }
+    }
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
-function InsertData(url, answer, testID, uid=1) {
+function InsertData(url, answer, testID, uid = 1) {
     fetch('/meres', {
         method: 'POST',
         body: JSON.stringify({
@@ -62,34 +54,26 @@ function InsertData(url, answer, testID, uid=1) {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         }
-
     })
-    .then ( response => response.json() )
+        .then(response => response.json());
 }
 
 function GetText(text) {
-    console.log("Current: " + text)
+    console.log("Current: " + text);
 }
 
 function fetchData() {
     return fetch('/api', {})
-            .then(response =>
-                response.json());
+        .then(response =>
+            response.json());
 }
-
-//fetchData().then(arr => console.log(arr[1]));
-
-//console.log(document.getElementById("mnistImage").src)
-//console.log(document.getElementById("mnistImage").src.charAt(0))
 
 function GetImageName() {
     filename = document.getElementById("mnistImage").src.replace(/^.*[\\\/]/, '').charAt(0);
-    return filename
-    //console.log(filename)
+    return filename;
 }
 
 function GetFirstLetter(str) {
-    str = str.split('\\').pop().split('/').pop().charAt(0)
-    return str
-    //console.log(filename)
+    str = str.split('\\').pop().split('/').pop().charAt(0);
+    return str;
 }
